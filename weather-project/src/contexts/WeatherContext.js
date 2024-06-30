460ba1da02b182b182347ad3a646cb8488521c97
thwnd <sojungh@hknu.ac.kr>
2024-07-01 00:13:25 +0900

    sibal aanhae

--- weather-project/src/contexts/WeatherContext.js
+++ weather-project/src/contexts/WeatherContext.js
@@ -2,28 +2,35 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 const WeatherContext = createContext();
 
-const weatherReducer = (state, action) => {
+const initialState = {
+  weatherData: null,
+  location: { lat: 37.5665, lon: 126.9780 }, // Default to Seoul
+  units: 'metric',
+};
+
+function weatherReducer(state, action) {
   switch (action.type) {
     case 'SET_WEATHER_DATA':
       return { ...state, weatherData: action.payload };
     case 'SET_LOCATION':
       return { ...state, location: action.payload };
+    case 'SET_UNITS':
+      return { ...state, units: action.payload };
     default:
       return state;
   }
-};
+}
 
-export const WeatherProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(weatherReducer, {
-    weatherData: null,
-    location: null
-  });
+export function WeatherProvider({ children }) {
+  const [state, dispatch] = useReducer(weatherReducer, initialState);
 
   return (
     <WeatherContext.Provider value={{ state, dispatch }}>
       {children}
     </WeatherContext.Provider>
   );
-};
+}
 
-export const useWeather = () => useContext(WeatherContext);
+export function useWeather() {
+  return useContext(WeatherContext);
+}
